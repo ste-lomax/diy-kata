3522174b5a4e665ba8b8b94a8456bda33b715d62
Steven Lomax <ste.lomax@gmail.com>
2022-03-01 17:42:18 +0000

    booleanToWord

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,11 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  it('returns yes if boolean true', () => {
+    expect(booleanToWord(true)).toEqual("Yes")
+  })
+
+  it('returns no if boolean false', () => {
+    expect(booleanToWord(false)).toEqual("No")
+  })
 });
