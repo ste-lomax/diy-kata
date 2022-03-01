59ededc33d936a39305dee99b62be87fb0840269
Steven Lomax <ste.lomax@gmail.com>
2022-03-01 22:49:20 +0000

    joinNames

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,13 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {  
+    return namesObj.reduce((acc, person, index) => {
+        if (index < namesObj.length -2) {
+            return acc + person.name + ", "
+        } else if (index < namesObj.length -1) {
+            return acc + person.name + " "
+        } else {
+            return acc + '& ' + person.name + "."
+        }
+    }, '')
+};
 
 module.exports = joinNames;
