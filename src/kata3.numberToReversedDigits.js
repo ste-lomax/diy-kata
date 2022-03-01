34760b1d6e87d56366bca224803031db23944c30
Steven Lomax <ste.lomax@gmail.com>
2022-03-01 18:23:00 +0000

    booleanToWordnumbersToReversedDigits

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,5 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = number => {
+    return number.toString().split("").reverse().map(Number);
+};
 
 module.exports = numberToReversedDigits;
