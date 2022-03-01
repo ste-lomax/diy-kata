8ad96ebd22071e8136c367c63a866ad286ae56fa
Steven Lomax <ste.lomax@gmail.com>
2022-03-01 23:38:40 +0000

    getEmployerRole

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,26 @@
-const getEmployerRole = (employeeName, employees) => {};
+const employees = [{
+
+        name: 'Satti',
+      
+        role: 'Developer'
+      
+      }, {
+      
+        name: 'Jenny',
+      
+        role: 'Sales Associate'
+      
+      }, {
+      
+        name: 'Javid',
+      
+        role: 'Human Recommended Reading Assistant'
+      
+      }]
+
+const getEmployerRole = (employeeName, employees) => {
+    const nameRole = employees.filter(obj => obj.name === employeeName);
+    return nameRole[0].role
+};
 
 module.exports = getEmployerRole;
--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,8 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  test("returns the employee's role in the company", () => {
+    expect(getEmployerRole('Javid', [{name: 'Satti', role: 'Developer'}, {name: 'Jenny', role: 'Sales Associate'}, {name: 'Javid',role: 'Human Recommended Reading Assistant'}])).toBe('Human Recommended Reading Assistant');
+    expect(getEmployerRole('Satti', [{name: 'Satti', role: 'Developer'}, {name: 'Jenny', role: 'Sales Associate'}, {name: 'Javid',role: 'Human Recommended Reading Assistant'}])).toBe('Developer');
+  });
 });
